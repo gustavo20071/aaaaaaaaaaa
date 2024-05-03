0f92aa9af74525d3c22e009c8e2d1abb620ff1eb
gustavo20071 <162706703+gustavo20071@users.noreply.github.com>
2024-05-03 09:41:20 -0300

    5

--- main.js
+++ main.js
@@ -7,8 +7,14 @@ const botoes = document.querySelectorAll('.parametro-senha__botao');
 console.log(botoes)
 
 botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
     tamanhoSenha = tamanhoSenha-1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+function aumentaTamanho(){
+    tamanhoSenha = tamanhoSenha+1;
     numeroSenha.textContent = tamanhoSenha;
 }

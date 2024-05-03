24c962fee35e5f6bc452c2e87a250bf3f691f62b
gustavo20071 <162706703+gustavo20071@users.noreply.github.com>
2024-05-03 10:29:06 -0300

    9

--- main.js
+++ main.js
@@ -26,20 +26,3 @@ function aumentaTamanho(){
     geraSenha();
 }
 
-
-const campoSenha = document.querySelector('#campo-senha');
-
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
-geraSenha();
-
-campoSenha.value = letrasMaiusculas;
-
-function geraSenha(){
-    let senha = '' ”;
-    for (let i = 0; i < tamanhoSenha;i++){
-        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
-        numeroAleatorio = Math.floor(numeroAleatorio);
-        senha = senha + letrasMaiusculas[numeroAleatorio];
-    }
-    campoSenha.value = senha;
-}
